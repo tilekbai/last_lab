aadb50eb0f519adf906afacbc4ff6bfd0f56ad7a
tilekbai <kukushokk@gmail.com>
2021-05-27 20:41:56 +0600

    Создано представление для создания цитаты

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
 
+from .serializers import *
 # Create your views here.
 
+
+class QuoteCreateView(generics.CreateAPIView):
+    serializer_class = QuoteDetailSerializer
