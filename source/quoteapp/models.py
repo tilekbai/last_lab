7443aac7452a0639871cafec67773019f7bcfa58
tilekbai <kukushokk@gmail.com>
2021-05-27 20:31:35 +0600

    Создана модель Quote

--- source/quoteapp/models.py
+++ source/quoteapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.enums import Choices
 
-# Create your models here.
+STATUS_CHOICE = (
+    ("новая", "новая"),
+    ("модерированная", "модерированная")    
+)
+
+class Quote(models.Model):
+    text = models.TextField(max_length=1000, blank=False, null=False, verbose_name="Текст")
+    name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Имя")
+    email = models.EmailField(blank=False, null=False, verbose_name="Email")
+    rating = models.IntegerField(default=0, verbose_name="Рэйтинг")
+    status = models.CharField(max_length=50, default="новая", choices=STATUS_CHOICE, verbose_name="Статус")
+    created = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"<<<{self.id}-{self.text}-{self.name}>>>"
+
