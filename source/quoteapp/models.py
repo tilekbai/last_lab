12b105838beb72cc6ac7b148346b7d6959730f56
tilekbai <kukushokk@gmail.com>
2021-05-27 21:14:37 +0600

    Добавлен choice для рэйтинга и прогнана миграция

--- source/quoteapp/models.py
+++ source/quoteapp/models.py
@@ -3,14 +3,23 @@ from django.db.models.enums import Choices
 
 STATUS_CHOICE = (
     ("новая", "новая"),
-    ("модерированная", "модерированная")    
+    ("модерированная", "модерированная")
 )
 
+
 class Quote(models.Model):
+    class RatingChoice(models.IntegerChoices):
+        ZERO = 0
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FORE = 4
+        FIVE = 5
+
     text = models.TextField(max_length=1000, blank=False, null=False, verbose_name="Текст")
     name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Имя")
     email = models.EmailField(blank=False, null=False, verbose_name="Email")
-    rating = models.IntegerField(default=0, verbose_name="Рэйтинг")
+    rating = models.IntegerField(choices=RatingChoice.choices, default=0, verbose_name="Рэйтинг")
     status = models.CharField(max_length=50, default="новая", choices=STATUS_CHOICE, verbose_name="Статус")
     created = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
 
