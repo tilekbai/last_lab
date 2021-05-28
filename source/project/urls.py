7c677b030ab75f0f7559665d24d44e4a17090a5c
tilekbai <kukushokk@gmail.com>
2021-05-28 11:06:30 +0600

    Добавлен просмотр главной страницы

--- source/project/urls.py
+++ source/project/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from quoteapp.views import Indexview
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Indexview, name='index'),
     path('', include('api_v1.urls', namespace='api_v1')),
-    path('', include('accounts.urls', namespace='accounts'))
+    path('', include('accounts.urls', namespace='accounts')),
 ]
