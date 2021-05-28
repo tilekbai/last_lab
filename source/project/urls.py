97185a7c494f0eb65393f20227a94ae14e576937
tilekbai <kukushokk@gmail.com>
2021-05-28 10:45:50 +0600

    Добавлен вход и выход в аккаунт urls.py

--- source/project/urls.py
+++ source/project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_v1.urls', namespace='api_v1'))
+    path('', include('api_v1.urls', namespace='api_v1')),
+    path('', include('accounts.urls', namespace='accounts'))
 ]
